0a6eb8e4b4553296a57ac4337c7e589bcf41e315
Prabhat Kumar Sardar <ps7188345@gmail.com>
2024-06-17 17:55:41 +0545

    Delete java directory

--- java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javaprojectlib {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javaprojectlib to javafx.fxml;
-    exports com.example.javaprojectlib;
-}
